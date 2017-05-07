d5d82d92b2c30d068ff966d660730083fbfd6daf
dbrusic <dbrusic@yahoo.com>
2017-05-06 22:19:23 -0700

    add plots

---
+++ plot1.R
@@ -0,0 +1,41 @@
+## download the data and unzip it
+if(!file.exists("./1plotdata")) {dir.create("./1plotdata")}
+temp <- tempfile()
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = temp)
+unzip(temp, exdir = "./1plotdata")
+unlink(temp)
+setwd("./1plotdata")
+
+
+## read data into R
+power <- read.table(file = "household_power_consumption.txt",
+                    header = TRUE,
+                    sep = ";", 
+                    na.strings = "?")
+
+## change "Date" column to the Date class
+power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
+
+## use dplyr functions to get only the data that you need
+## also, combine "Date" and "Time" columns into one column called "datetime"
+library(dplyr)
+power <- filter(power, Date == "2007-02-01" | Date == "2007-02-02") 
+power <- mutate(power, datetime = paste(power$Date, power$Time, sep = " ")) %>% 
+        select(datetime, Global_active_power:Sub_metering_3)
+
+## convert datetime to POSIXct
+power$datetime <- as.POSIXct(power$datetime)
+
+## plot historgram of global active power
+png(file = "plot1.png", width = 480, height = 480)
+with(power, hist(Global_active_power, col = "red", main = "Global Active Power",
+                 xlab = "Global Active Power (kilowatts)"))
+dev.off()
+
+
+
+
+
+
+
---
+++ plot4.R
@@ -0,0 +1,52 @@
+## download the data and unzip it
+if(!file.exists("./1plotdata")) {dir.create("./1plotdata")}
+temp <- tempfile()
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = temp)
+unzip(temp, exdir = "./1plotdata")
+unlink(temp)
+setwd("./1plotdata")
+
+
+## read data into R
+power <- read.table(file = "household_power_consumption.txt",
+                    header = TRUE,
+                    sep = ";", 
+                    na.strings = "?")
+
+## change "Date" column to the Date class
+power$Date <- as.Date(power$Date, format = "%d/%m/%Y")
+
+## use dplyr functions to get only the data that you need
+## also, combine "Date" and "Time" columns into one column called "datetime"
+library(dplyr)
+power <- filter(power, Date == "2007-02-01" | Date == "2007-02-02") 
+power <- mutate(power, datetime = paste(power$Date, power$Time, sep = " ")) %>% 
+        select(datetime, Global_active_power:Sub_metering_3)
+
+## convert datetime to POSIXct
+power$datetime <- as.POSIXct(power$datetime)
+
+## plot
+png(file = "plot4.png", width = 480, height = 480)
+par(mfcol = c(2, 2))
+## PLOT 1
+with(power, {
+        plot(datetime, Global_active_power, type = "l", xlab = "", 
+                 ylab = "Global Active Power")
+## PLOT 2
+        plot(datetime, Sub_metering_1, type = "n", 
+                 ylab = "Energy sub metering", xlab = "")
+        points(datetime, Sub_metering_1, type = "l")
+        points(datetime, Sub_metering_2, type = "l", col = "red")
+        points(datetime, Sub_metering_3, type = "l", col = "blue")
+        legend("topright", col = c("black", "red", "blue"),
+        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+        lty = c(1, 1, 1),
+        bty = "n")
+## PLOT 3
+        plot(datetime, Voltage, type = "l")
+## PLOT 4
+        plot(datetime, Global_reactive_power, type = "l")
+})
+dev.off()
